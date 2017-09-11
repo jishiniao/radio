e7a5fa67219fa99d467d0108a21e42d3fdefe9ca
yangkang <28302551@qq.com>
2017-09-11 17:48:20 +0800

    Test GitHub Contributions

--- src/Radio.js
+++ src/Radio.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export default class Radio extends React.Component {
+
+  // static propTypes = {
+  //
+  // }
+
   render() {
     return <h1>Hello, Radio!</h1>
   }
