a9d6cdd42bb16cf7be412e60aa749423cdbb7841
yangkang <28302551@qq.com>
2017-09-12 10:07:01 +0800

    Test publish npm

--- src/Radio.js
+++ src/Radio.js
@@ -3,11 +3,19 @@ import PropTypes from 'prop-types'
 
 export default class Radio extends React.Component {
 
-  // static propTypes = {
-  //
-  // }
-
   render() {
     return <h1>Hello, Radio!</h1>
   }
+}
+
+Radio.defaultProps = {
+
+}
+
+Radio.PropTypes = {
+  type: PropTypes.string,
+  names: PropTypes.array,
+  values: PropTypes.array,
+  onClick : PropTypes.func,
+  icons: PropTypes.array,
 }
